9a1df3afda8f1bc481851b67e7aa4f51a805eede
Nico96C <nicolas.cuello96@hotmail.com>
2023-09-11 15:59:29 -0300

    Agregado codigo Clase Conexion

--- src/universidadgrupo51/accesoAdatos/Conexion.java
+++ src/universidadgrupo51/accesoAdatos/Conexion.java
@@ -5,10 +5,41 @@
  */
 package universidadgrupo51.accesoAdatos;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Admin
  */
 public class Conexion {
     
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "universidad";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+    
+    private Conexion(){
+        
+    }
+    
+    public static Connection getConexion(){
+        
+        if (connection == null) {
+            
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL+DB, USUARIO, PASSWORD);
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar los Drivers " + ex.getMessage());
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a la Base de Datos " + DB + " "  + ex.getMessage());
+            }
+            
+        }
+        return connection;
+    }
 }
